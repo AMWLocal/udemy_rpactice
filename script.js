b37dea7ed79bd30aaac8a53ee6d53b2d8088ab57
usachewandrei <usachewandrei@gmail.com>
2020-11-23 20:51:15 +0200

    add first function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const nunberOfFilms = +prompt('Сколько фильмов вы посмотрели', '')
+let nunberOfFilms;
+
 const personalMovieDB = {
     count: nunberOfFilms,
     movies: {},
@@ -6,10 +7,38 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('один из последних фиьльмов', '')
-const b = prompt('На сколько оцените его', '')
-const c = prompt('один из последних фиьльмов', '')
-const d = prompt('На сколько оцените его', '')
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB) 
+
+function start() {
+    nunberOfFilms = +prompt('Сколько фильмов вы посмотрели', '')
+    while (nunberOfFilms == '' || nunberOfFilms == null || isNaN(nunberOfFilms)) {
+        nunberOfFilms = +prompt('Сколько фильмов вы посмотрели', '')
+
+    }
+};
+start()
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('один из последних фиьльмов', '')
+        const b = prompt('На сколько оцените его', '');
+        if (a !== null && b != null && b != '' && a != '') {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error')
+            i--;
+        }
+    }
+    console.log(personalMovieDB)
+}
+rememberMyFilms()
+
+function isPrivat() {
+    if (personalMovieDB.privat) {
+        console.log('is privat')
+    } else {
+        console.log('is public')
+
+    }
+}
+isPrivat()
+
